bdf9e5ce794e492ad5f0be05cff28cb04513204c
ZmijaZ <zmijaz2001@gmail.com>
2023-07-27 16:35:46 +0200

    initial commit, add basic components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>Courtesy of @ZmijaZ</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <div>
+      <h1>Cards:</h1>
+    </div>
+  );
+};
+
+export default Main;
